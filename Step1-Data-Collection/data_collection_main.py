0aae4b80daa2a7e114d42cc2f615591a7c7f9a60
Muhammad Almorshidi <muhammad.almorshidi@gmail.com>
2024-06-18 12:33:07 +0300

    Update

--- Step1-Data-Collection/data_collection_main.py
+++ Step1-Data-Collection/data_collection_main.py
@@ -5,50 +5,56 @@ Data Collection Main Program
 This script controls a car's movement using key presses and records data while the car is in motion.
 
 It initializes modules for data collection, key press detection, and the PiCamera module for image capture.
-The script continuously monitors key presses to adjust the car's speed and steering angle.
+The script continuously monitors key presses to adjust the car's speed and steering angle .
 Key presses trigger various actions, such as adjusting speed and angle, starting and stopping recording, and terminating the program.
 Recorded data includes images and corresponding speed and steering angle.
 
 Modules:
 --------
-- data_collection_module: Handles data collection and saving.
-- key_press_module: Manages key press detection and control.
-- uart_module: Manages UART communication for sending speed and angle data to the microcontroller.
-- picamera_module: Interfaces with the Raspberry Pi Camera for image capture.
+- data_collection_module    : Handles data collection and saving.
+- key_press_module          : Manages key press detection and control.
+- uart_module               : Manages UART communication for sending control commands.
+- picamera_module           : Interfaces with the Raspberry Pi Camera for image capture.
 
 Global Variables:
 -----------------
-- done: Flag variable to terminate the program.
-- record: Flag variable to control recording status.
-- key_val: Last key pressed.
-- key_press_t, key_press_t_1: Dictionaries to track key press status.
+- done      : Flag variable to terminate the program.
+- record    : Flag variable to control recording status.
+- key_val   : Current pressed key.
+- key_old   : Last pressed key
+- speed     : Speed
+- angle     : Steering angle
 
 Functions:
 ----------
-- main(): Main function to control the car's movement, handle key presses, and manage data recording.
+- get_key_press()               : Get key press status and update global variables.
+- update_movement_controls()    : Update speed and angle based on key presses.
+- send_movement_commands()      : Send movement commands over UART based on current speed and angle.
+- main()                        : Main function to control the car's movement, handle key presses, and manage data recording.
+
 
 Key Presses:
 ------------
-- RIGHT: Steer right.
-- LEFT: Steer left.
-- UP: Increase speed.
-- DOWN: Decrease speed.
-- r: Start or stop recording.
-- s: Stop the car.
-- k: Terminate the program.
+- RIGHT : Increase speed and steer right.
+- LEFT  : Increase speed and steer left.
+- UP    : Increase speed.
+- DOWN  : Decrease speed.
+- r     : Start or stop recording.
+- s     : Stop the car.
+- k     : Terminate the program.
 
 Example Usage:
 --------------
 To run the script, ensure the required modules (data_collection_module, key_press_module, uart_module, picamera_module) are imported and available in the environment.
 
-    $ python data_collection_main.py
+    $ python3 data_collection_main.py
 
 Dependencies:
 -------------
-- data_collection_module: Ensure that the `data_collection_module` module is properly implemented and available.
-- key_press_module: Ensure that the `key_press_module` module is properly implemented and available.
-- uart_module: Ensure that the `uart_module` module is properly implemented and available.
-- picamera_module: Ensure that the `picamera_module` module is properly implemented and available.
+- data_collection_module    : Ensure that the `data_collection_module` module is properly implemented and available.
+- key_press_module          : Ensure that the `key_press_module` module is properly implemented and available.
+- uart_module               : Ensure that the `uart_module` module is properly implemented and available.
+- picamera_module           : Ensure that the `picamera_module` module is properly implemented and available.
 
 Note:
 -----
@@ -59,22 +65,22 @@ This script is designed to control an autonomous car system and requires proper 
 import os
 from data_collection_module import DataCollector
 from key_press_module       import KeyPressController
-from picamera_module        import PiCameraController
 from uart_module            import UartController
+from picamera_module        import PiCameraController
 
 # Constants
 KEY_LIST = ["RIGHT", "LEFT", "UP", "DOWN", "r", "s", "k"]
 SERIAL_PORT = "/dev/ttyS0"
 BAUD_RATE = 9600
+ROI = (0.0, 0.2, 0.8, 0.8) # Ratio of interest
 
 # Global Variables
-done            = 0                                 # Flag variable to terminate the program
-record          = 0                                 # Flag variable to control recording status
-key_val         = KEY_LIST[0]                       # Last key pressed
-key_press_t_1   = {key: False for key in KEY_LIST}  # Dictionary to track key press status (previous)
-key_press_t     = {key: False for key in KEY_LIST}  # Dictionary to track key press status (current)
-speed           = 0                                 # Initial speed
-angle           = 0                                 # Initial steering angle
+done    = 0     # Flag variable to terminate the program
+record  = 0     # Flag variable to control recording status
+key_val = None  # Current pressed key
+key_old = None  # Last pressed key
+speed   = 0     # Initial speed
+angle   = 0     # Initial steering angle
 
 # Initializing modules
 data_collector = DataCollector()
@@ -86,14 +92,13 @@ key_controller.key_press_init()
 uart_controller = UartController(SERIAL_PORT, BAUD_RATE)
 
 camera_controller = PiCameraController()
-roi = (0.0, 0.2, 0.8, 0.8) #ratio of interest
-camera_controller.pi_cam_init(roi)
+camera_controller.pi_cam_init(ROI)
 
 def get_key_press():
     """
     Get key press status and update global variables.
     
-    This function checks the status of each key in the KEY_LIST and updates the key_val and key_press_t variables accordingly.
+    This function checks the status of each key in the KEY_LIST and updates the key_val accordingly.
     
     Args:
         None
@@ -105,16 +110,13 @@ def get_key_press():
     for key in KEY_LIST:
         if key_controller.get_key_status(key):
             key_val = key
-            key_press_t[key] = True
             break
-        else:
-            key_press_t[key] = False
 
 def update_movement_controls():
     """
     Update speed and angle based on key presses.
     
-    This function updates the speed and angle variables based on the last key pressed.
+    This function updates the speed and angle variables based on the current pressed key.
     
     Args:
         None
@@ -122,37 +124,53 @@ def update_movement_controls():
     Returns:
         None
     """
-    global speed, angle, record, done
-    if (key_press_t[key_val] != key_press_t_1[key_val]):
-        key_press_t_1[key_val] = key_press_t[key_val]
-    else:
-        return
-    if (key_press_t[key_val] == True):
-        key_press_t_1[key_val]
-        print(key_val)
-        if key_val == "RIGHT":
-            angle += 0.2
-        elif key_val == "LEFT":
-            angle -= 0.2
-        elif key_val == "UP":
-            speed += 0.2
-            angle = 0
-        elif key_val == "DOWN":
-            speed -= 0.2
-            angle = 0
-        elif key_val == "r":
-            record += 1
-        elif key_val == "s":
-            speed = 0
-            angle = 0
-        elif key_val == "k":
-            done += 1
+    global speed, angle, record, done, key_val, key_old
+    if key_val == "RIGHT":
+        speed = 0.6
+        angle = speed
+    elif key_val == "LEFT":
+        speed = 0.6
+        angle = -speed
+    elif key_val == "UP":
+        speed = 0.4
+        angle = 0
+    elif key_val == "DOWN":
+        speed = -0.4
+        angle = 0
+    elif key_val == "s":
+        speed = 0
+        angle = 0
+    elif key_val == "k":
+        done += 1
+    elif key_val == key_old:
+        pass
+    elif key_val == "r":
+        key_old = key_val
+        record += 1
+    if key_val != "r":
+        key_val = None
+        key_old = None
+
+def send_movement_commands():
+    """
+    Send movement commands over UART based on current speed and angle.
+    
+    This function formats the speed and angle into a string and sends it over UART.
+    
+    Args:
+        None
+    
+    Returns:
+        None
+    """
+    command = f"{speed},{angle}"
+    uart_controller.send_data(command)
 
 def main():
     """
     Main function to control the car's movement.
     
-    This function continuously monitors key presses, updates movement controls, sends movement controls to the microcontroller, and manages data recording.
+    This function continuously monitors key presses, updates movement controls, and manages data recording.
     
     Args:
         None
@@ -160,15 +178,12 @@ def main():
     Returns:
         None
     """
-    global speed, angle, record, done
+    global speed, angle, record, done, key_val, key_old
     while True:
+        angle = 0
         get_key_press()
         update_movement_controls()
-
-        # Send speed and angle over UART
-        uart_message = f"{speed},{angle}"
-        uart_controller.send_data(uart_message)
-        print(f"Sent over UART: {uart_message}")
+        send_movement_commands()
 
         # Start recording
         if record == 1:
@@ -186,10 +201,12 @@ def main():
             record = 0
             data_collector.save_log()
             data_collector.img_list.clear()
+            data_collector.speed_list.clear()
             data_collector.angle_list.clear()
 
         # Terminate program
         if done != 0:
+            uart_controller.send_data("stop")
             uart_controller.close()
             break
 
--- Step3-Implementation/autonomous_car_controller.py
+++ Step3-Implementation/autonomous_car_controller.py
@@ -9,19 +9,20 @@ The script continuously captures images, processes them, and uses a machine lear
 
 Modules:
 --------
-- uart_module: Manages UART communication for sending speed and angle data.
-- picamera_module: Interfaces with the Raspberry Pi Camera for image capture.
+- uart_module       : Manages UART communication for sending speed and angle data.
+- picamera_module   : Interfaces with the Raspberry Pi Camera for image capture.
 
 Global Variables:
 -----------------
-- uart_controller Instance of UartController for UART communication.
-- camera: Instance of PiCameraController to capture images.
-- model: Pre-trained machine learning model for speed and angle prediction.
+- speed : Initial speed.
+- angle : Initial steering angle.
+- model : Pre-trained machine learning model for speed and angle prediction.
 
 Functions:
 ----------
-- preProcess(img): Preprocess the captured image for the model.
-- main(): Main function to capture images, predict speed and angle, and send control commands over UART.
+- preProcess(img)           : Preprocess the captured image for the model.
+- send_movement_commands()  : Send movement commands over UART based on current speed and angle.
+- main()                    : Main function to capture images, predict speed and angle, and send control commands over UART.
 
 Example Usage:
 --------------
@@ -43,21 +44,26 @@ This script is designed to control an autonomous car system and requires proper 
 import os
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
-from uart_module import UartController
-from picamera_module import PiCameraController
+from tensorflow.keras.models    import load_model
+from uart_module                import UartController
+from picamera_module            import PiCameraController
+
+# Constants
+SERIAL_PORT = "/dev/ttyS0"
+BAUD_RATE   = 9600
+ROI         = (0.0, 0.2, 0.8, 0.8)  # Ratio of interest
+MODEL_PATH  = "path_to_your_model.h5"  # Update with your model path
+
+# Global Variables
+speed = 0  # Initial speed
+angle = 0  # Initial steering angle
+model = load_model(MODEL_PATH)
 
 # Initializing modules
-SERIAL_PORT = "/dev/ttyS0"
-BAUD_RATE = 9600
 uart_controller = UartController(SERIAL_PORT, BAUD_RATE)
 
 camera_controller = PiCameraController()
-roi = (0.0, 0.2, 0.8, 0.8) #ratio of interest
-camera_controller.pi_cam_init(roi)
-
-model_path = None
-model = load_model(model_path)
+camera_controller.pi_cam_init(ROI)
 
 def preProcess(img):
     """
@@ -75,28 +81,51 @@ def preProcess(img):
     img = img / 255.0  # Normalize the image
     return img
 
+def send_movement_commands():
+    """
+    Send movement commands over UART based on current speed and angle.
+    
+    This function formats the speed and angle into a string and sends it over UART.
+    
+    Args:
+        None
+    
+    Returns:
+        None
+    """
+    command = f"{speed},{angle}"
+    uart_controller.send_data(command)
+
 def main():
     """
     Main function to capture images, predict speed and angle, and send control commands over UART.
 
     This function continuously captures images, processes them, and uses a pre-trained model to predict the speed and steering angle to control the car's movement.
+    
+    Args:
+        None
+    
+    Returns:
+        None
     """
-    img_path = os.path.join(os.getcwd(), "road_img")
-    while True:
-        camera.get_img(img_path)  # Capture image
-        img = cv2.imread(img_path)
-        img = np.asarray(img)  # Convert to numpy array
-        img = preProcess(img)  # Preprocess the image
-        img = np.expand_dims(img, axis=0)  # Add batch dimension
-        prediction = model.predict(img)  # Predict angle and speed
-        angle = float(prediction[0][0])  # Extract angle
-        speed = float(prediction[0][1])  # Extract speed
+    global speed, angle, model
+    try:
+        while True:
+            img_path = camera_controller.get_img()  # Capture image and get path
+            img = cv2.imread(img_path)
+            img = np.asarray(img)  # Convert to numpy array
+            img = preProcess(img)  # Preprocess the image
+            img = np.expand_dims(img, axis=0)  # Add batch dimension
+            prediction = model.predict(img)  # Predict angle and speed
+            angle = float(prediction[0][0])  # Extract angle
+            speed = float(prediction[0][1])  # Extract speed
 
-        print(f"Angle: {angle}, Speed: {speed}")  # Print values
-        uart_message = f"{speed},{angle}"  # Create UART message
-        uart_controller.send_data(uart_message)  # Send data over UART
-        print(f"Sent over UART: {uart_message}")  # Print UART message
-        cv2.waitKey(1)  # Wait for 1 ms
+            print(f"Angle: {angle}, Speed: {speed}")  # Print values
+            send_movement_commands()
+            cv2.waitKey(1)  # Wait for 1 ms
+    except KeyboardInterrupt:
+        uart_controller.send_data("stop")
+        uart_controller.close()
 
 if __name__ == "__main__":
     main()
